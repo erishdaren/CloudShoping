6cce957df854ae5d93f39526732413487fea44d4
Erick <moreno240190@gmail.com>
2019-11-19 20:53:48 -0600

    Inicio de Proyecto

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div>
+      
+        <Header />
+    </div>
+  );
+}
+
+export default App;
+// instale el modulo de router video 3
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <div>
+            <header>
+                <img src="/img/logo.png" />
+            </header>
+        </div>
+    )
+}
